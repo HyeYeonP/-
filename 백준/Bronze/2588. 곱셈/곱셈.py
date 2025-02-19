75d1b767a93aaa6e051367f6c6180f0f245e6392
HyeYeonP <110224345+HyeYeonP@users.noreply.github.com>
2025-02-19 15:47:41 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,7 +1,8 @@
 a = int(input())
-b = input()
-b_1 = int(b[2])
-b_2 = int(b[1])
-b_3 = int(b[0])
-b_4 = int(b)
-print(a*b_1, a*b_2, a*b_3, a*b_4, sep='\n')
+b = int(input())
+
+b_1 = b%10
+b_2 = b//10%10
+b_3 = b//100
+
+print(a*b_1, a*b_2, a*b_3, a*b, sep='\n')
