4f9bde856b3195c1fd5196c70b6a41998615556d
HyeYeonP <110224345+HyeYeonP@users.noreply.github.com>
2025-03-10 10:45:25 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+a = list(range(1, n+1))
+
+for _ in range(m):
+  i, j = map(int, input().split())
+  a[i-1], a[j-1] = a[j-1], a[i-1]
+
+for b in range(n):
+  print(a[b], end = ' ')
