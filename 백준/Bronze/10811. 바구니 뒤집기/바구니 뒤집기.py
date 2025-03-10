df7dfc631991627b14bb624626a9405333504e6c
HyeYeonP <110224345+HyeYeonP@users.noreply.github.com>
2025-03-10 11:11:27 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+a = list(range(1, n+1))
+
+for _ in range(m):
+  i, j = map(int, input().split())
+  a[i-1:j] = reversed(a[i-1:j])
+
+for b in range(n):
+  print(a[b], end = ' ') 
